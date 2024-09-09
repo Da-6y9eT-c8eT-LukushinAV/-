fdc7ef8b5a36fd9c7e869eeb9f32f816a74e2a61
Da-6y9eT-c8eT-LukushinAV <lukushinav@mer.ci.nsu.ru>
2024-09-09 14:15:46 +0700

    Add files via upload

---
+++ Polymorph/Polymorph/CustomConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorph
+{
+    internal class CustomConverter
+    {
+        public static bool Convert(string value, out int result)
+        {
+            return int.TryParse(value, out result);
+        }
+
+        public static bool Convert(string value, out double result)
+        {
+            return double.TryParse(value, out result);
+        }
+
+        public static bool Convert(string value, out bool result)
+        {
+            return bool.TryParse(value, out result);
+        }
+
+        public static bool Convert(string value, out DateTime result)
+        {
+            return DateTime.TryParse(value, out result);
+        }
+
+        public static bool Convert(string value, out long result)
+        {
+            return long.TryParse(value, out result);
+        }
+    }
+}
---
+++ Polymorph/Polymorph/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Polymorph
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Примеры вызова методов Convert
+            if (CustomConverter.Convert("123", out int intResult))
+            {
+                Console.WriteLine($"intResult: {intResult}");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось преобразовать строку в int");
+            }
+
+            if (CustomConverter.Convert("0,34", out double doubleResult))
+            {
+                Console.WriteLine($"doubleResult: {doubleResult}");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось преобразовать строку в double");
+            }
+
+            if (CustomConverter.Convert("true", out bool boolResult))
+            {
+                Console.WriteLine($"boolResult: {boolResult}");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось преобразовать строку в bool");
+            }
+
+            if (CustomConverter.Convert("2024-09-09", out DateTime dateTimeResult))
+            {
+                Console.WriteLine($"dateTimeResult: {dateTimeResult}");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось преобразовать строку в DateTime");
+            }
+
+            if (CustomConverter.Convert("1234567890123", out long longResult))
+            {
+                Console.WriteLine($"longResult: {longResult}");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось преобразовать строку в long");
+            }
+        }
+    }
+}
