768c4ddf5083f7d1b4d8b7de3252355cd3fd8d42
Da-6y9eT-c8eT-LukushinAV <lukushinav@mer.ci.nsu.ru>
2024-09-24 16:56:45 +0700

    Add files via upload

---
+++ FactoryMet/FactoryMet/Student.cs
@@ -0,0 +1,28 @@
+﻿using FactoryMet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMet
+{
+    public class Student : Entity
+    {
+        public List<int> CourseIds { get; } = new List<int>();
+
+        public Student(int id, string name) : base(id, name)
+        {
+        }
+
+        public bool AddCourse(int courseId)
+        {
+            if (!CourseIds.Contains(courseId))
+            {
+                CourseIds.Add(courseId);
+                return true;
+            }
+            return false;
+        }
+    }
+}
